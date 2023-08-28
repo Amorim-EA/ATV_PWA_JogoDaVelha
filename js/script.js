c6540a87fc2e242bbd5bff198fa7cde687baec34
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-28 17:41:22 -0400

    Interface do usuario criada, front ok, falta as funçoes de validação e etc do jogo.

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let row1col1 = document.getElementById("row1col1");
+let row1col2 = document.getElementById("row1col2");
+let row1col3 = document.getElementById("row1col3");
+let row2col1 = document.getElementById("row2col1");
+let row2col2 = document.getElementById("row2col2");
+let row2col3 = document.getElementById("row2col3");
+let row3col1 = document.getElementById("row3col1");
+let row3col2 = document.getElementById("row3col2");
+let row3col3 = document.getElementById("row3col3");
+
+row1col1.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row1col2.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row1col3.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row2col1.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row2col2.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row2col3.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row3col1.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row3col2.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
+
+row3col3.addEventListener('click', () => {
+    console("row1col1 clicado")
+});
